31842fc8a1834792308aa6d7ab96df446d8b232f
zhamman <zhamman20@gmail.com>
2022-01-31 00:02:17 -0800

    adjested all the burger menus on the project page and adjusted some of spacing issues when shrinking the page view

--- project-page3/project3.js
+++ project-page3/project3.js
@@ -2,17 +2,20 @@ const navSlide = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav');
     const navLinks = document.querySelectorAll('.nav li');
-
+    const line2 = document.querySelector(".hamburger .line2");
 
     burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active')
+        line2.classList.toggle("turn")
+        burger.classList.toggle("rotate");
+        nav.classList.toggle('nav-active');
         navLinks.forEach((link, idx)=>{
-            link.style.animation = `navLinkFade 1s ease forwards ${idx/ 7 + 1}s`
-        })
+            link.style.animation = `navLinkFade 0.3s ease forwards ${idx/ 7 + 0.3}s`
+        });
     });
 }
 
 navSlide()
+
 
 // hamburger.addEventListener('click', () =>{
 //     navUL.classList('.nav-active')
