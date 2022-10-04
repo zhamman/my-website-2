aaffe58875070e1acadf8fc9e39b7012a9687689
Zach Hamman <zhamman20@gmail.com>
2022-10-03 20:01:10 -0700

    Changed contact button, contact form now functional, links to project sites now working.

--- app.js
+++ app.js
@@ -1,67 +1,86 @@
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-let subTitle = document.querySelector('.sub-title');
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo");
+let subTitle = document.querySelector(".sub-title");
 let subLink = document.querySelector(".sub a");
-let headerLinks = document.querySelector('.nav li')
+let headerLinks = document.querySelector(".nav li");
+const btn = document.getElementById("sbm-button");
 
-// Opening page animation 
-window.addEventListener('DOMContentLoaded', ()=>{
+// Opening page animation
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
 
-    setTimeout(()=>{
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
 
-        logoSpan.forEach((span, idx) => {
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        })
-           
-        setTimeout(()=>{
-            logoSpan.forEach((span,idx) => {    
-
-                setTimeout(() => {
-                span.classList.remove('active')
-                span.classList.add('fade')
-                }, (idx + 1) * 50)
-            })
-        },2000);
-
-        setTimeout(()=>{
-            intro.style.top = '-100vh'
-        }, 2000)
-    })
-})
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2000);
+  });
+});
 
 // Front page Animations
 
 // subtitle animation
 
-setTimeout(function(){
-    subTitle.style.visibility = 'visible'; 
+setTimeout(function () {
+  subTitle.style.visibility = "visible";
 }, 4200);
 
-setTimeout(function(){
-    subLink.style.visibility = 'visible'; 
+setTimeout(function () {
+  subLink.style.visibility = "visible";
 }, 4200);
- 
+
 //  Hamburger menu fade in
 
 const navSlide = () => {
-    const burger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav');
-    const navLinks = document.querySelectorAll('.nav li');
-    const line2 = document.querySelector(".hamburger .line2");
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+  const navLinks = document.querySelectorAll(".nav li");
+  const line2 = document.querySelector(".hamburger .line2");
 
-    burger.addEventListener('click', () =>{
-        line2.classList.toggle("turn")
-        burger.classList.toggle("rotate");
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, idx)=>{
-            link.style.animation = `navLinkFade 0.3s ease forwards ${idx/ 7 + 0.3}s`
-        });
+  burger.addEventListener("click", () => {
+    line2.classList.toggle("turn");
+    burger.classList.toggle("rotate");
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, idx) => {
+      link.style.animation = `navLinkFade 0.3s ease forwards ${idx / 7 + 0.3}s`;
     });
-}
+  });
+};
 
-navSlide()
+navSlide();
 
+// form Submit
 
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "service_npet1q3";
+  const templateID = "template_1reyw7s";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      document.getElementById("form").innerHTML =
+        "Your Message has been sent. Thank You!";
+    },
+    err => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
