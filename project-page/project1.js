0fd83b319a46755388f58be9c8ff0d61f562cc0d
Zach Hamman <zhamman20@gmail.com>
2024-01-19 16:34:49 -0800

    changes to code, refactor, styling update, clean up

--- project-page/project1.js
+++ project-page/project1.js
@@ -1,21 +1,17 @@
 const navSlide = () => {
-    const burger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav');
-    const navLinks = document.querySelectorAll('.nav li');
-    const line2 = document.querySelector(".hamburger .line2");
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+  const navLinks = document.querySelectorAll(".nav li");
+  const line2 = document.querySelector(".hamburger .line2");
 
-    burger.addEventListener('click', () =>{
-        line2.classList.toggle("turn")
-        burger.classList.toggle("rotate");
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, idx)=>{
-            link.style.animation = `navLinkFade 0.3s ease forwards ${idx/ 7 + 0.3}s`
-        });
+  burger.addEventListener("click", () => {
+    line2.classList.toggle("turn");
+    burger.classList.toggle("rotate");
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, idx) => {
+      link.style.animation = `navLinkFade 0.3s ease forwards ${idx / 7 + 0.3}s`;
     });
-}
+  });
+};
 
-navSlide()
-
-// hamburger.addEventListener('click', () =>{
-//     navUL.classList('.nav-active')
-// });
+navSlide();
